de5dcd4360bf0fa3aca1636032e15d858ddee1fb
Tareq0Hasan <tareq1363@gmail.com>
2023-01-19 09:40:51 +0600

    dbconfig

--- support_prac/DBconfig.js
+++ support_prac/DBconfig.js
@@ -2,6 +2,7 @@ const { MongoClient } = require("mongodb");
 require('dotenv').config()
 const client = new MongoClient(process.env.DATABASE);
 
+
 // console.log('database connected successfully')
 
 module.exports={client}
